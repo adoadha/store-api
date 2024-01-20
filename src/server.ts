69a05830ef70d1f27ddcd7c6e53feecb5748a357
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2024-01-20 19:10:00 +0700

    fix: upload image to cloudinary

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,16 @@ import fastifyCors from "@fastify/cors";
 import Ajv from "ajv";
 import "dotenv/config";
 import fastify, { FastifyInstance, FastifyRequest } from "fastify";
-import { join } from "path";
+import path, { join } from "path";
 import config from "./constant/config";
 import fastifyCloudinary from "fastify-cloudinary";
 import fastifyMultipart from "@fastify/multipart";
-import { v2 as cloudinary } from "cloudinary";
+import cloudinary from "cloudinary";
+import stream from "stream";
+import fs from "fs";
+import util from "util";
 
-interface FileData {
-  file: {
-    fieldname: string;
-    filename: string;
-    encoding: string;
-    mimetype: string;
-    tempFilePath: string;
-    size: number;
-  };
-}
+const pipeline = util.promisify(stream.pipeline);
 
 const server = fastify({
   ajv: { customOptions: { coerceTypes: "array" } },
@@ -63,8 +57,6 @@ server.addHook("onResponse", (request, _reply, done) => {
   done();
 });
 
-server.register(fastifyMultipart, { attachFieldsToBody: true });
-
 server.setErrorHandler(function (error, request, reply) {
   reply.code(error.statusCode ?? 0).send({ ...error });
 });
@@ -93,66 +85,32 @@ server.register(fastifyCors, () => {
   };
 });
 
+server.register(fastifyMultipart);
 server.register(fastifyCloudinary, {
-  // cloudinary: {
-  //   cloudName: config.CLOUD_NAME,
-  //   apiKey: config.CLOUD_API_KEY,
-  //   apiSecret: config.CLOUD_API_SECRET,
-  // },
   url: `cloudinary://${config.CLOUD_API_KEY}:${config.CLOUD_API_SECRET}@${config.CLOUD_NAME}`,
 });
 
-// server.post("/upload", async (request, reply) => {
-//   try {
-//     const data: any = request.file;
+cloudinary.v2.config({
+  cloudName: config.CLOUD_NAME,
+  apiKey: config.CLOUD_API_KEY,
+  apiSecret: config.CLOUD_API_SECRET,
+});
 
-//     if (!data) {
-//       console.log(request.file, "data");
-//     }
+server.post("/upload", async (request: FastifyRequest, reply) => {
+  try {
+    const data = await request.file();
 
-//     const result = await cloudinary.uploader.upload(data.file, {
-//       folder: "folder_upload_anda", // Tentukan folder di Cloudinary
-//     });
-
-//     const imageUrl = result.secure_url;
-
-//     return { imageUrl };
-//   } catch (err) {
-//     reply.code(500).send(err);
-//   }
-// });
-
-server.route({
-  url: "/upload",
-  method: "POST",
-  handler: async (request: FastifyRequest, reply) => {
-    try {
-      const data: any = await request.file();
-
-      // console.log(JSON.stringify(data));
-
-      if (!data || !data.file) {
-        reply.code(400).send({ error: "No file uploaded" });
-        return;
-      }
-
-      // @ts-nocheck
-      const result = await server.cloudinary.uploader.upload(
-        data.file.tempFilePath,
-        {
-          public_id: `${Date.now()}`,
-          resource_type: "auto",
-        }
+    if (data) {
+      await pipeline(
+        data.file,
+        server.cloudinary.uploader.upload_stream({ public_id: data.fieldname })
       );
-      console.log("DISINI");
 
-      const imageUrl = result.secure_url;
-
-      // return { imageUrl };
-    } catch (err) {
-      reply.code(500).send(err);
+      return reply.send({ url: server.cloudinary.url(data.fieldname) });
     }
-  },
+  } catch (err) {
+    reply.code(500).send(err);
+  }
 });
 
 export const app: FastifyInstance = server;
